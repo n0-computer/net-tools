f63fa5f3fb1a455cf5e7bf1c5f643ac77dafe9a3
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-02 19:01:57 +0000

    deploy: ce12d352ed0282006b5eaa478cf2a194acc9b78a

---
+++ pr/1/docs/trait.impl/struct_iterable_internal/trait.Iterable.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"portmapper":[["impl Iterable for <a class=\"struct\" href=\"portmapper/struct.Metrics.html\" title=\"struct portmapper::Metrics\">Metrics</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
