c7e39eb8a21a881ced3b226223dc1448247f598d
flub <flub@users.noreply.github.com>
2025-01-08 18:53:44 +0000

    deploy: 87fb820b0b77982099a8791f4737a1dc7fb6c6b4

---
+++ pr/8/docs/trait.impl/iroh_metrics/core/trait.Metric.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["portmapper",[["impl Metric for <a class=\"struct\" href=\"portmapper/struct.Metrics.html\" title=\"struct portmapper::Metrics\">Metrics</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[145]}
