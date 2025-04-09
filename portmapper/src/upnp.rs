c04c68d3624e32f8bb682442db63cf467b1a938c
Frando <frando@unbiskant.org>
2025-04-09 10:35:18 +0200

    feat: update to latest iroh-metrics branch

--- portmapper/src/upnp.rs
+++ portmapper/src/upnp.rs
@@ -1,11 +1,11 @@
 use std::{
     net::{Ipv4Addr, SocketAddrV4},
     num::NonZeroU16,
+    sync::Arc,
     time::Duration,
 };
 
 use igd_next::{aio as aigd, AddAnyPortError, GetExternalIpError, RemovePortError, SearchError};
-use iroh_metrics::inc;
 use tracing::debug;
 
 use super::Metrics;
@@ -149,8 +149,8 @@ impl Mapping {
 }
 
 /// Searches for UPnP gateways.
-pub async fn probe_available() -> Option<Gateway> {
-    inc!(Metrics, upnp_probes);
+pub async fn probe_available(metrics: &Arc<Metrics>) -> Option<Gateway> {
+    metrics.upnp_probes.inc();
 
     // Wrap in manual timeout, because igd_next doesn't respect the set timeout
     let res = tokio::time::timeout(
@@ -165,12 +165,12 @@ pub async fn probe_available() -> Option<Gateway> {
     match res {
         Ok(Ok(gateway)) => Some(gateway),
         Err(e) => {
-            inc!(Metrics, upnp_probes_failed);
+            metrics.upnp_probes_failed.inc();
             debug!("upnp probe timed out: {e}");
             None
         }
         Ok(Err(e)) => {
-            inc!(Metrics, upnp_probes_failed);
+            metrics.upnp_probes_failed.inc();
             debug!("upnp probe failed: {e}");
             None
         }
