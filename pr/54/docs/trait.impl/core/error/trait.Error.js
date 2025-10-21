be3c869715d1dce253768aca91eec325472e5d76
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-10-21 10:34:41 +0000

    deploy: 1921f63c2d8f282c85223b95b9627ab772ecda05

---
+++ pr/54/docs/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["netwatch",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"netwatch/netmon/enum.Error.html\" title=\"enum netwatch::netmon::Error\">Error</a><div class=\"where\">where\n    Self: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>"]]],["portmapper",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"portmapper/enum.ProbeError.html\" title=\"enum portmapper::ProbeError\">ProbeError</a><div class=\"where\">where\n    Self: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[584,591]}
