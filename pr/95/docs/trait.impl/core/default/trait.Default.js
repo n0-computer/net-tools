b1e1941df0e5c21e00fb0e383d3ab04b1d00992d
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-02-06 12:12:47 +0000

    deploy: a3593871f18ad67660828943d02fc061fdb6f2cf

---
+++ pr/95/docs/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["netwatch",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"netwatch/ip/struct.LocalAddresses.html\" title=\"struct netwatch::ip::LocalAddresses\">LocalAddresses</a>"]]],["portmapper",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"portmapper/struct.Client.html\" title=\"struct portmapper::Client\">Client</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"portmapper/struct.Config.html\" title=\"struct portmapper::Config\">Config</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"portmapper/struct.Metrics.html\" title=\"struct portmapper::Metrics\">Metrics</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[305,812]}
