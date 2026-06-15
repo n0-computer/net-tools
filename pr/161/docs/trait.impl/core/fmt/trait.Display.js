571d049a1e3a9eda6688384474a218d5daf6b116
rklaehn <rklaehn@users.noreply.github.com>
2026-06-15 09:07:17 +0000

    deploy: a0456e3488702d598fd5accaaef09e41ea4dee14

---
+++ pr/161/docs/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["netwatch",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"netwatch/netmon/enum.Error.html\" title=\"enum netwatch::netmon::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"netwatch/interfaces/struct.Interface.html\" title=\"struct netwatch::interfaces::Interface\">Interface</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"netwatch/interfaces/struct.State.html\" title=\"struct netwatch::interfaces::State\">State</a>"]]],["portmapper",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"portmapper/enum.ProbeError.html\" title=\"enum portmapper::ProbeError\">ProbeError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"portmapper/struct.ProbeOutput.html\" title=\"struct portmapper::ProbeOutput\">ProbeOutput</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[828,550]}
