771fb334a401d4c96e47d5e5c5f59c47ac36c271
flub <flub@users.noreply.github.com>
2026-07-16 16:27:32 +0000

    deploy: a417809765de5cbb753fab1040f7ece62a623f68

---
+++ pr/185/docs/trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["netwatch",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"netwatch/enum.IpFamily.html\" title=\"enum netwatch::IpFamily\">IpFamily</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"netwatch/interfaces/struct.Ipv6AddrFlags.html\" title=\"struct netwatch::interfaces::Ipv6AddrFlags\">Ipv6AddrFlags</a>"]]],["portmapper",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"portmapper/enum.Protocol.html\" title=\"enum portmapper::Protocol\">Protocol</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[556,270]}
