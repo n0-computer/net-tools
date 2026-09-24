ceb2acca885824f73d9062658f0335fc5c99e739
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-09-24 12:29:12 +0000

    deploy: 467a790f9d92a519eac928a7b6633a7e2492cd09

---
+++ pr/234/docs/trait.impl/std/os/fd/owned/trait.AsFd.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["netwatch",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"netwatch/struct.SocketRef.html\" title=\"struct netwatch::SocketRef\">SocketRef</a>&lt;'_&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[292]}
